e0b420dbbeca57fc07f5f9f5994c327e168fb76b
Igor Souza <153653125+Isouzah@users.noreply.github.com>
2025-06-14 00:51:55 +0000

    terceiro desafio

---
+++ ope_mat.py
@@ -0,0 +1,13 @@
+num1 = int(input("Coloque o primeiro numero: "))
+num2 = int(input("Coloque o segundo numero: "))
+
+operador = input("Escolha o operador [+ - * /]: ")
+
+if operador == "+":
+    print(num1 + num2),
+elif operador == "-":
+    print(num1 - num2),
+elif operador == "*":
+    print(num1 * num2),
+elif operador == "/":
+    print(num1 / num2)
